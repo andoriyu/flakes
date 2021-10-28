9b3bf5f66f3d13930998b2b2ef67cc3a8c20d9fd
Andrey Cherkashin <andrey@dave.com>
2021-10-28 11:45:28 -0700

    fix overlay

--- overlay.nix
+++ overlay.nix
@@ -1,4 +1,5 @@
-{ final, prev }: {
+final: prev:
+{
   cargo-expand-nightly = import ./packages/cargo-expand { inherit final prev; };
   git-cliff = import ./packages/git-cliff { inherit final prev; };
 }
