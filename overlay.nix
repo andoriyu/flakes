caae3c0967eac0864115a6d4e983036776efbbd6
Andrey Cherkashin <148123+andoriyu@users.noreply.github.com>
2022-01-25 13:34:10 -0800

    Update overlay.nix

--- overlay.nix
+++ overlay.nix
@@ -2,7 +2,7 @@ final: prev:
 {
   cargo-expand-nightly = import ./packages/cargo-expand { inherit final prev; };
   git-cliff = import ./packages/git-cliff { inherit final prev; };
-  dart-sass = final.callPackage ./packages/rust-analyzer/default.nix {
+  dart-sass = final.callPackage ./packages/dart-sass/default.nix {
     version = "1.49.0";
     sha256 = "701a1896383ac06f545f4fc70c0c945b98cff3d041d7dc44f322db8addcc1167";
   };
