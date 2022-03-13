d9f634ee9868e6616d4e2c6ad5c6bac3c2f7fea7
Andrey Cherkashin <andoriyu@gmail.com>
2022-03-12 17:13:35 -0800

    Update dart-sass to 1.49.9

--- overlay.nix
+++ overlay.nix
@@ -3,6 +3,14 @@ final: prev:
   cargo-expand-nightly = import ./packages/cargo-expand { inherit final prev; };
   git-cliff = import ./packages/git-cliff { inherit final prev; };
   dart-sass = final.callPackage ./packages/dart-sass/default.nix {
+    version = "1.49.9";
+    sha256 = "sha256-lsA3QK5namROVlgrCWDTLISQjeSWYn48IjjuJJdNlsY=";
+  };
+  "dart-sass-1.49" = final.callPackage ./packages/dart-sass/default.nix {
+    version = "1.49.9";
+    sha256 = "sha256-lsA3QK5namROVlgrCWDTLISQjeSWYn48IjjuJJdNlsY=";
+  };
+  "dart-sass-1.49.0" = final.callPackage ./packages/dart-sass/default.nix {
     version = "1.49.0";
     sha256 = "701a1896383ac06f545f4fc70c0c945b98cff3d041d7dc44f322db8addcc1167";
   };
