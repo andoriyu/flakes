7213f192154467c52fdb9be068ad2661a379bbd4
Andrey Snow <148123+andoriyu@users.noreply.github.com>
2022-07-16 11:54:37 -0700

    package(doctave): Add doctave 0.4.2 (#58)

--- packages/doctave/default.nix
+++ packages/doctave/default.nix
@@ -12,7 +12,12 @@ rustPlatform.buildRustPackage rec {
     sha256 = "sha256-8mGSFQozyLoGua9mwyqfDcYNMtbeWp9Phb0vaje+AJ0=";
   };
 
-  cargoSha256 = "sha256-W/I5UilHXjFtmwialnAOyI3M1XBU2CSv7XwGbXje4HQ=";
+  cargoPatches = [
+    ./patches/update-Cargo.lock.patch
+  ];
+
+  cargoSha256 = "sha256-keLcNttdM9JUnn3qi/bWkcObIHl3MRACDHKPSZuScOc=
+e";
   meta = with lib; {
     description = "Doctave is an opinionated documentation site generator that converts your Markdown files into a beautiful documentation site with minimal effort.";
     homepage = "https://github.com/doctave/doctave";
