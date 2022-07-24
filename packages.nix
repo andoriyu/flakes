1eb34d3c935a36f8ca39bf8878982122d6f48693
Andrey Cherkashin <andoriyu@gmail.com>
2022-07-24 17:11:46 +0000

    packages(dart-sass): Add 1.54.0 version

--- packages.nix
+++ packages.nix
@@ -11,7 +11,7 @@ let
 in
 rec {
   atlas = pkgs.callPackage ./packages/atlas/default.nix { };
-  dart-sass = dart-sass-1_53_0;
+  dart-sass = dart-sass-1_54_0;
   git-cliff = pkgs.callPackage ./packages/git-cliff { rustPlatform = rustPlatformStable; };
   doctave = pkgs.callPackage ./packages/doctave { rustPlatform = rustPlatformStable; };
   cargo-expand-nightly = pkgs.callPackage ./packages/cargo-expand { toolchain = rustNightlyToolchain; };
@@ -23,6 +23,9 @@ rec {
   };
   dart-sass-1_53_0 = pkgs.callPackage ./packages/dart-sass {
     version = "1.53.0";
+  };
+  dart-sass-1_54_0 = pkgs.callPackage ./packages/dart-sass {
+    version = "1.54.0";
   };
   strongdm-cli = pkgs.callPackage ./packages/sdm-cli {
     version = "33.57.0";
