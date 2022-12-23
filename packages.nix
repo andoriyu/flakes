60cd2ad964a932f0ba9fc09aa6eea3ec2837230d
Andrey Cherkashin <andoriyu@gmail.com>
2022-12-23 19:30:33 +0000

    fmt: run nixfmt

--- packages.nix
+++ packages.nix
@@ -12,33 +12,33 @@ in
 rec {
   atlas = pkgs.callPackage ./packages/atlas/default.nix { };
   dart-sass = dart-sass-1_57_1;
-  git-cliff = pkgs.callPackage ./packages/git-cliff { rustPlatform = rustPlatformStable; };
-  doctave = pkgs.callPackage ./packages/doctave { rustPlatform = rustPlatformStable; };
-  cargo-expand-nightly = pkgs.callPackage ./packages/cargo-expand { toolchain = rustNightlyToolchain; };
-  dart-sass-1_52_1 = pkgs.callPackage ./packages/dart-sass {
-    version = "1.52.1";
+  git-cliff = pkgs.callPackage ./packages/git-cliff {
+    rustPlatform = rustPlatformStable;
   };
-  dart-sass-1_52_2 = pkgs.callPackage ./packages/dart-sass {
-    version = "1.52.2";
+  doctave =
+    pkgs.callPackage ./packages/doctave { rustPlatform = rustPlatformStable; };
+  cargo-expand-nightly = pkgs.callPackage ./packages/cargo-expand {
+    toolchain = rustNightlyToolchain;
   };
-  dart-sass-1_53_0 = pkgs.callPackage ./packages/dart-sass {
-    version = "1.53.0";
-  };
-  dart-sass-1_54_4 = pkgs.callPackage ./packages/dart-sass {
-    version = "1.54.4";
-  };
-  dart-sass-1_57_1 = pkgs.callPackage ./packages/dart-sass {
-    version = "1.57.1";
-  };
+  dart-sass-1_52_1 =
+    pkgs.callPackage ./packages/dart-sass { version = "1.52.1"; };
+  dart-sass-1_52_2 =
+    pkgs.callPackage ./packages/dart-sass { version = "1.52.2"; };
+  dart-sass-1_53_0 =
+    pkgs.callPackage ./packages/dart-sass { version = "1.53.0"; };
+  dart-sass-1_54_4 =
+    pkgs.callPackage ./packages/dart-sass { version = "1.54.4"; };
+  dart-sass-1_57_1 =
+    pkgs.callPackage ./packages/dart-sass { version = "1.57.1"; };
 
-  strongdm-cli = pkgs.callPackage ./packages/sdm-cli {
-    version = "33.57.0";
-  };
+  strongdm-cli = pkgs.callPackage ./packages/sdm-cli { version = "33.57.0"; };
 } // pkgs.lib.optionalAttrs pkgs.stdenv.isLinux {
   City = pkgs.callPackage ./packages/city-theme { };
   st-onedark = st_0_8_14.overrideAttrs (oldAttrs: rec {
     configFile = ./packages/st/config.def.h-onedark;
-    postPatch = "${oldAttrs.postPatch}\n cp ${configFile} config.def.h";
+    postPatch = ''
+      ${oldAttrs.postPatch}
+       cp ${configFile} config.def.h'';
     buildInputs = oldAttrs.buildInputs ++ [ pkgs.harfbuzz ];
   });
 }
