1931b85e587452eb3b3acdd1b2a572731f9188f5
Andrey Cherkashin <andoriyu@gmail.com>
2023-02-01 18:17:56 +0000

    package(dart-sass): Add dart-sass 1.58.0

---
+++ packages/dart-sass-snapshot/make_version.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+cd $(dirname "$0")
+
+declare -A PLATFORMS=(
+    [macos-x64]=x86_64-darwin
+    [macos-arm64]=aarch64-darwin
+    [linux-x64]=x86_64-linux
+    [linux-arm64]=aarch64-linux
+)
+
+BASE="https://github.com/sass/dart-sass/releases/download"
+VERSION=$1
+TMP=versions.tmp.json
+
+
+echo '' > $TMP
+
+for platform in "${!PLATFORMS[@]}"; do
+  # https://github.com/sass/dart-sass/releases/download/1.52.1/dart-sass-1.52.1-linux-arm64.tar.gz"
+  url="${BASE}/${VERSION}/dart-sass-${VERSION}-${platform}.tar.gz"
+  sha256=$(nix store prefetch-file --json "${url}" | jq -r '.hash')
+  nixPlatform=${PLATFORMS[$platform]}
+  jq --arg version ${VERSION} \
+    --arg platform $nixPlatform \
+    --arg url "$url" \
+    --arg sha256 $sha256 \
+    -n '$ARGS.named' >> $TMP
+done
+
+jq -s '.' $TMP > $TMP.arr
+jq -s '.[0] + .[1]' $TMP.arr versions.json > versions-new.json
+mv versions-new.json versions.json
+rm $TMP $TMP.arr
+cd -
---
+++ packages/dart-sass/split.nix
@@ -0,0 +1,36 @@
+# Copyright 2020 Kyryl Vlasov
+# SPDX-License-Identifier: MIT
+
+{ stdenv, fetchurl, binutils, version, lib }:
+
+let
+  inherit (stdenv.hostPlatform) system;
+
+  meta =
+    let
+      versions = builtins.fromJSON (builtins.readFile ./versions.json);
+      matches = x: x.version == version && x.platform == system;
+    in
+    lib.findFirst matches (throw "Don't know this version yet") versions;
+in
+stdenv.mkDerivation {
+  name = "dart-sass-${version}";
+  inherit version;
+
+  isExecutable = true;
+
+  src = fetchurl { inherit (meta) sha256 url; };
+  phases = "unpackPhase installPhase fixupPhase";
+
+  sourceRoot = "./dart-sass";
+  installPhase = ''
+    install -m755 -D sass $out/bin/sass
+    install -m755 -D src/dart $out/bin/src/dart
+    install -m755 -D src/sass.snapshot $out/bin/src/sass.snapshot
+  '';
+  fixupPhase = ''
+    patchelf \
+        --set-interpreter ${binutils.dynamicLinker} \
+        $out/bin/src/dart
+  '';
+}
